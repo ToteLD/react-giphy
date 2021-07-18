3ee720152701c8f1fedb874f37b76854aa0b4545
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-07-18 20:26:58 +0200

    add list to right scene

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Gif from './gif.jsx';
+import List from './list.jsx';
 import SearchBar from './search_bar.jsx';
 
 class App extends Component {
     render() {
+        const gifs = [
+            { id: "aagX4Bl8Fo21G"},
+            { id: "3o6wNYTRSluicbit5S"}
+        ]
+
         return(
             <div>
                 <div className="left-scene">
@@ -12,7 +18,9 @@ class App extends Component {
                         <Gif />
                     </div>
                 </div>
-                <div className="right-scene"></div>
+                <div className="right-scene">
+                    <List gifs={gifs} />
+                </div>
             </div>
         );
     }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Gif extends Component {
 
     render() {
-        const src = "https://media4.giphy.com/media/aagX4Bl8Fo21G/giphy.gif?cid=ecf05e47c1183f4435ee73b63bd3533bfd6f9e6f7f888d3f&rid=giphy.gif&ct=g"
+        const src = `https://media4.giphy.com/media/${this.props.id}/200.gif`;
         return(
             <img src={src} alt="" className="gif" />
         )
