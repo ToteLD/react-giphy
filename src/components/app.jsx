5ce852d4f6836bc6fdbe4966698486178e6ee848
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-07-20 14:25:34 +0200

    add giphy api

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,24 +2,43 @@ import React, { Component } from 'react';
 import Gif from './gif.jsx';
 import List from './list.jsx';
 import SearchBar from './search_bar.jsx';
+import giphy from 'giphy-api';
+import { result } from 'lodash';
 
 class App extends Component {
-    render() {
-        const gifs = [
-            { id: "aagX4Bl8Fo21G"},
-            { id: "3o6wNYTRSluicbit5S"}
-        ]
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            gifs: [],
+            selectedGifId: "aagX4Bl8Fo21G"
+        }
+
+        this.search("homer thinking");
+    }
+    
+    search = (query) => {
+        giphy('1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm').search({
+            q: query,
+            rating: 'g'
+    }, (err, res) => {
+            this.setState({
+                gifs: res.data
+            });
+        });
+    }
+
+    render() {
         return(
             <div>
                 <div className="left-scene">
                     <SearchBar />
                     <div className="selected-gif">
-                        <Gif />
+                        <Gif id={this.state.selectedGifId}/>
                     </div>
                 </div>
                 <div className="right-scene">
-                    <List gifs={gifs} />
+                    <List gifs={this.state.gifs} />
                 </div>
             </div>
         );
